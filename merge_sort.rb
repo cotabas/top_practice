9b3b6459e20e8c1dd327239528ac87f08d289e1e
cotabas <cotabas@gmail.com>
2022-05-23 19:11:05 -0700

    finally got the merge sort! hard to get my mind around.. but I understand recursion much better now

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,18 +1,27 @@
-def merge_sort(array, answer = [], left_answer = [], right_answer = [])
-  p array.flatten
-  if array.size <= 2
-    return answer << (array[0] >= array[1] ? [array[1], array[0]] : [array[0], array[1]])
-  end
+def merge_sort(array, left = [], right = [])
+  # Why is the assignment branch condition size for merge_sort too high?
+  
+  return [array[0]] if array.size == 1
 
-  left = array.size.odd? ? (array.size + 1) / 2 : array.size / 2
-  right = array.size - left
-  left_answer << merge_sort(array.slice(0, left), answer, left_answer, right_answer)
-  right_answer << merge_sort(array.slice(-right, right), answer, left_answer, right_answer)
+  left_side = array.slice(0, (array.size.odd? ? (array.size + 1) / 2 : array.size / 2))
+  right_side = array.slice(-(array.size - left_side.size), (array.size - left_side.size))
 
-  answer << (left_answer[0] < right_answer[0] ? left_answer[0] : right_answer[0])
+  left = merge_sort(left_side, left, right)
+  right = merge_sort(right_side, left, right)
 
-  return answer.flatten
+  merge(left, right)
 end
 
-p merge_sort([2,1,3,4,6,5])
+def merge(left, right, answer = [])
+  (left.size + right.size).times do
+    break answer << left if right.empty?
+    break answer << right if left.empty?
+
+    answer << (left[0] > right[0] ? right.slice!(0, 1) : left.slice!(0, 1))
+  end
+
+  answer.flatten
+end
+
+p merge_sort([2,9,1,7,6,29,456,457,5,4,2,4,6,5,7,23,24,25,26,21,20])
 
