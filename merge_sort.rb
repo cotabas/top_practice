f1b91fe9e6a5ec3526157f31edcd4a180d2f2a06
cotabas <cotabas@gmail.com>
2022-05-23 19:27:53 -0700

    One more refinement..

--- merge_sort.rb
+++ merge_sort.rb
@@ -20,5 +20,6 @@ def merge(left, right, answer = [])
   answer.flatten
 end
 
-p merge_sort([2,9,1,7,6,29,456,457,5,4,2,4,6,5,7,23,24,25,26,21,20])
-
+test_array = [2,9,1,7,6,29,456,457,5,4,2,4,6,5,7,23,24,25,26,21,20]
+p "Before: #{test_array}"
+p "After: #{merge_sort(test_array)}"
