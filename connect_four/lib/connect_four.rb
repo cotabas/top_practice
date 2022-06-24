c17abf001674acef77de86ad66a648fe03d7eae0
cotabas <cotabas@gmail.com>
2022-06-23 21:46:42 -0700

    Small start, out of time

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -5,4 +5,16 @@ class Board
   def initialize
     @board = Array.new(6, Array.new(7, nil))
   end
+
+  def check_winner(board_state)
+    check_horizontal(board_state)
+  end
+
+  def check_horizontal(board_state)
+    board_state.each do |row| 
+      return 'x' if row.include?(['x','x','x','x'])
+    end
+    false
+  end
+
 end
--- connect_four/spec/test_suite.rb
+++ connect_four/spec/test_suite.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/connect_four'
 
+def make_board
+  Array.new(6, Array.new(7, nil))
+end
+
 describe Board do
   describe '#initialize' do
     subject(:game) { described_class.new }
@@ -14,5 +18,18 @@ describe Board do
         expect(board[0].length).to eq(7)
       end
     end
+    context 'Checks for horizontal winners' do
+      it 'four in a row on the bottom row' do
+        board_state = make_board
+        board_state[0] = [nil, nil, 'x', 'x', 'x', 'x', nil]
+        expect(game.check_winner(board_state)).to eq('x')
+      end
+      it 'four not in a row on the bottom' do 
+        board_state = make_board
+        board_state[0] = ['x', nil, 'x', nil, 'x', 'x', nil]
+        expect(game.check_winner(board_state)).not_to eq('x')
+      end
+    end
+
   end
 end
