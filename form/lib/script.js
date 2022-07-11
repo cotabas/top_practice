6a6ce9132da0e3ac91ee76fa0501839951b92ac0
cotabas <cotabas@gmail.com>
2022-07-11 14:49:35 -0700

    added the pass check with js. project done.

--- form/lib/script.js
+++ form/lib/script.js
@@ -1,13 +1,24 @@
-const subButt = document.querySelector('#createAccount');
-const pass = document.querySelector('#pass').value;
-const confirmPass = document.querySelector('#confirmPass').value;
+
 const passMatch = document.querySelector('#passMatch');
+const theForm = document.querySelector('#theForm');
 
-subButt.addEventListener('click', passCheck);
+theForm.addEventListener('submit', passCheck);
 
-function passCheck() {
-  if (pass != confirmPass) {
+function passCheck(form) {
+  const whatAPain = form.target;
+
+  if (theForm.pass.value != theForm.confirmPass.value) {  
     passMatch.style.visibility = 'visible';
   }
+  if (whatAPain.checkValidity()) {
+    if (theForm.pass.value != theForm.confirmPass.value) {  
+      passMatch.style.visibility = 'visible';
+      form.preventDefault();
+    }
+  } else {
+    form.preventDefault();
+  }
+ 
+  
   
 }
